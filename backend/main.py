22a62d32025b5ab3b0570cad4cc56d498e8c5074
Lakshya-Kapoor <Lakshya.Kapoor@iiitb.ac.in>
2025-05-21 23:55:12 +0530

    implement login and signup pages

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,7 @@ from environs import Env
 from contextlib import asynccontextmanager
 from utils import connect_db
 from routers import auth_router, admin_router, user_router
+from fastapi.middleware.cors import CORSMiddleware
 
 
 @asynccontextmanager
@@ -14,6 +15,15 @@ async def lifespan(app: FastAPI):
     yield
 
 app = FastAPI(lifespan=lifespan)
+
+# Add CORS middleware to allow all origins, methods, and headers
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_router, prefix="/auth")
 app.include_router(admin_router, prefix="/admin")
